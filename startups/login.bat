4b3faf37a88bb4d9950fdf18da8275f59db69809
bengjerstad <bengjerstad@gmail.com>
2016-11-17 13:51:49 -0500

    Delete login.bat
    not needed for this project

--- startups/login.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-
-cscript /nologo %~dp0/wget.js "http://10.24.25.130:8000/log_this?username="+%USERNAME%+"&compname="+%computername%+"&stat=on&time="+%mydate%_%mytime%
