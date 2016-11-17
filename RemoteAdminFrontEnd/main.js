286ae8281181417de1952b13c934d3720a247e18
bengjerstad <bengjerstad@gmail.com>
2016-11-17 14:08:51 -0500

    Add files via upload
    Some test javascript that I was using to see if this idea was doable.

---
+++ RemoteAdminFrontEnd/main.js
@@ -0,0 +1,39 @@
+SERVERIP = '10.24.25.67:8080';
+$(document).ready(function(){
+    $("#mainnavbutton").click(function(){
+        $.getJSON("http://"+SERVERIP+"/connection", function(result){
+			$("#main").html('<ul>');
+			$("#stattxt").text('List of duplicate logs received.');
+			$.each( result, function( key, val ) {
+				username = key.split(" ")[0];
+				link = "<a href='#' class='dupitem'>"+username+ "</a>"
+				$("#main").append("<li>"+link+"</li>");
+			});
+			$("#main").append("</ul>");
+			$(".dupitem").click(function(){
+				username = $(this).text();
+				$(".selected").removeClass( "selected" );
+				$(this).addClass( "selected" );
+				$("#exusername").val(username);
+				$("#exnav").removeClass('hidden');
+				$.getJSON("http://"+SERVERIP+":8000/get_log?username="+username+"&compname=all", function(result){
+					$("#logview").html("");
+					$("#logview").append("<table>");
+					$("#stattxt").text('Log for user '+username+' received.');
+					$.each( result, function( key, val ) {
+					datetime = val.time;
+					datetime = datetime.split("_");
+					date = datetime[0].split("-");
+					date[0] = date[0].substring(1, 5);
+					date = date[1]+"/"+date[2]+"/"+date[0];
+					timeh = datetime[1].substring(0, 2);
+					timem = datetime[1].substring(2, 4);
+						$("#logview").append("<tr><td>"+val.username+" </td><td> "+val.compname+" </td><td> "+val.stat+" </td><td> "+timeh+":"+timem+" </td><td> "+date+" </td></tr>");
+					});
+				});
+				
+				
+			});
+        });
+    });
+});
